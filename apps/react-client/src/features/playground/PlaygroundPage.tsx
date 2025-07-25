d239dc91d5d32d437639200cad214fbaa6bd923b
synikolaev <synikolaev@inno.tech>
2025-07-25 13:21:07 +0300

    feat(playground): add CodeEditorDemo to playground components

--- apps/react-client/src/features/playground/PlaygroundPage.tsx
+++ apps/react-client/src/features/playground/PlaygroundPage.tsx
@@ -2,6 +2,7 @@ import { Typography } from "@mui/material";
 import { Card } from "@react-client/common/muiCustom/Card";
 import { Flex } from "@react-client/common/primitives/Flex";
 import { Spacer } from "@react-client/common/primitives/Spacer";
+import { CodeEditorDemo } from "@react-client/features/codeEditor";
 import { Header } from "@react-client/features/navigation/organisms/Header";
 import { JsonEditorSvelte } from "@react-client/features/playground/jsonEditor/organisms/JsonEditorSvelte";
 import { JsonEditorWithDiff } from "@react-client/features/playground/jsonEditor/organisms/JsonEditorWithDiff";
@@ -10,6 +11,7 @@ import { SyncDemo } from "@react-client/features/syncDemo";
 import { Fragment } from "react/jsx-runtime";
 
 const data = [
+	{ name: "CodeEditorDemo", Component: CodeEditorDemo },
 	{ name: "SyncDemo", Component: SyncDemo },
 	{ name: "JsonEditorWithDiff", Component: JsonEditorWithDiff },
 	{ name: "JsonEditorSvelte", Component: JsonEditorSvelte },
