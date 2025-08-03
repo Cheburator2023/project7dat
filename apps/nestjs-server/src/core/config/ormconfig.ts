ac17c182f503f0636d2db697cb010e23d8d88f78
Victor Gabbasov <borisborisov03102001@gmail.com>
2025-08-04 02:26:40 +0300

    feat: DRY

--- apps/nestjs-server/src/core/config/ormconfig.ts
+++ apps/nestjs-server/src/core/config/ormconfig.ts
@@ -1,33 +1,6 @@
-import { ConfigService } from "@nestjs/config";
-import { DataSource, DataSourceOptions } from "typeorm";
+import { DataSource } from 'typeorm';
+import databaseConfig from './database.config';
 
-const configService = new ConfigService();
+const config = databaseConfig();
 
-const getDataSourceOptionsForCLI = (): DataSourceOptions => {
-	const isProduction = configService.get("NODE_ENV") === "production";
-
-	if (isProduction) {
-		return {
-			type: "postgres",
-			host: configService.get("DB_HOST", "localhost"),
-			port: configService.get("DB_PORT", 5432),
-			username: configService.get("DB_USERNAME", "postgres"),
-			password: configService.get("DB_PASSWORD", "password"),
-			database: configService.get("DB_NAME", "data_lineage"),
-			entities: [__dirname + "/**/*.entity{.ts,.js}"],
-			migrations: [__dirname + "/migrations/*{.ts,.js}"],
-			synchronize: false,
-			logging: false,
-		};
-	}
-
-	return {
-		type: "sqlite",
-		database: "./dev-database.sqlite",
-		entities: [__dirname + "/**/*.entity{.ts,.js}"],
-		synchronize: true,
-		logging: true,
-	};
-};
-
-export default new DataSource(getDataSourceOptionsForCLI());
+export default new DataSource(config);
--- apps/nestjs-server/src/core/shared/database/providers/database-storage.provider.ts
+++ apps/nestjs-server/src/core/shared/database/providers/database-storage.provider.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource, EntityTarget, Repository, ObjectLiteral } from 'typeorm';
 import { IDatabaseProvider } from '../interfaces/database.interface';
+import databaseConfig from '../../../config/database.config';
 
 @Injectable()
 export class DatabaseProvider implements IDatabaseProvider {
@@ -10,25 +10,13 @@ export class DatabaseProvider implements IDatabaseProvider {
 
     constructor(private readonly configService: ConfigService) {}
 
-    getConfig(): TypeOrmModuleOptions {
-        return {
-            type: 'postgres',
-            host: this.configService.get<string>("DB_HOST", "localhost"),
-            port: this.configService.get<number>("DB_PORT", 5432),
-            username: this.configService.get<string>("DB_USERNAME", "postgres"),
-            password: this.configService.get<string>("DB_PASSWORD", "postgres"),
-            database: this.configService.get<string>("DB_NAME", "calculation_db"),
-            entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-            synchronize: this.configService.get('DB_SYNCHRONIZE') === 'true',
-            logging: this.configService.get('DB_LOGGING') === 'true',
-            migrations: [__dirname + '/../../../../migrations/*{.ts,.js}'],
-            migrationsRun: true,
-        };
+    getConfig() {
+        return databaseConfig();
     }
 
     async connect(): Promise<void> {
         if (!this.dataSource) {
-            this.dataSource = new DataSource(this.getConfig() as any);
+            this.dataSource = new DataSource(this.getConfig());
         }
         if (!this.dataSource.isInitialized) {
             await this.dataSource.initialize();
@@ -41,14 +29,18 @@ export class DatabaseProvider implements IDatabaseProvider {
         }
     }
 
-    getRepository<Entity extends ObjectLiteral>(entity: EntityTarget<Entity>): Repository<Entity> {
+    getRepository<Entity extends ObjectLiteral>(
+        entity: EntityTarget<Entity>,
+    ): Repository<Entity> {
         if (!this.dataSource) {
             throw new Error('Database not connected');
         }
         return this.dataSource.getRepository(entity);
     }
 
-    async transaction<T>(operation: (entityManager: any) => Promise<T>): Promise<T> {
+    async transaction<T>(
+        operation: (entityManager: any) => Promise<T>,
+    ): Promise<T> {
         if (!this.dataSource) {
             throw new Error('Database not connected');
         }
