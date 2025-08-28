bc687a00cf1fd6a762aba5b1f626a6695e9295d4
VGabbasov <VGabbasov@inno.tech>
2025-08-28 19:28:14 +0300

    feat:Поправил миграции согласно Разбор бд-92

--- apps/nestjs-server/src/migrations/1760000000010-CreateAttributeMapSourceTable.ts
+++ apps/nestjs-server/src/migrations/1760000000010-CreateAttributeMapSourceTable.ts
@@ -11,21 +11,22 @@ export class CreateAttributeMapSourceTable1760000000010 implements MigrationInte
                 change_id           INTEGER NOT NULL,
                 CONSTRAINT pk_attribute_map_source PRIMARY KEY (attribute_map_id, source_attribute_id),
                 CONSTRAINT fk_attribute_map_source_change FOREIGN KEY (change_id) REFERENCES changes (change_id),
-                CONSTRAINT fk_attribute_map_source_attribute FOREIGN KEY (source_attribute_id) REFERENCES attribute (attribute_id)
+                CONSTRAINT fk_attribute_map_source_attribute FOREIGN KEY (source_attribute_id) REFERENCES attribute (attribute_id),
+                CONSTRAINT fk_attribute_map_source_attribute_map FOREIGN KEY (attribute_map_id) REFERENCES attribute_map (attribute_map_id)
             )
         `);
         await queryRunner.query(`
             COMMENT ON TABLE attribute_map_source IS 'Маппинг атрибута источника (источник + идентификатор его маппинга на target)'
         `);
         await queryRunner.query(`
-            COMMENT ON COLUMN attribute_map_source.attribute_map_id IS 'Идентификатор записи'
+            COMMENT ON COLUMN attribute_map_source.attribute_map_id IS 'Идентификатор общего маппинга атрибута'
         `);
         await queryRunner.query(`
             COMMENT ON COLUMN attribute_map_source.source_attribute_id IS 'Идентификатор атрибута источника'
         `);
         await queryRunner.query(`
             COMMENT ON COLUMN attribute_map_source.change_id IS 'Идентификатор изменения'
-        `)
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
