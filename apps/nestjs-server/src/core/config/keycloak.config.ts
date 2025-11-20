a6e4897c2352bced5e9785a62b4da8b4195f888a
VGabbasov <VGabbasov@inno.tech>
2025-11-20 14:11:07 +0300

    feat: Убрал дублирование валидации JSON

--- apps/nestjs-server/src/core/config/keycloak.config.ts
+++ apps/nestjs-server/src/core/config/keycloak.config.ts
@@ -31,36 +31,4 @@ export const keycloakValidationSchema = Joi.object({
 		.valid("online", "offline", "none")
 		.default("offline")
 		.description("Тип валидации токена"),
-}).custom((value, helpers) => {
-	const noRoles = process.env.NO_ROLES === "true";
-
-	if (noRoles) {
-		return value;
-	}
-
-	const errors: string[] = [];
-
-	if (!value.KEYCLOAK_URL) {
-		errors.push('"KEYCLOAK_URL" is required');
-	}
-
-	if (!value.KEYCLOAK_REALM) {
-		errors.push('"KEYCLOAK_REALM" is required');
-	}
-
-	if (!value.KEYCLOAK_CLIENT_ID) {
-		errors.push('"KEYCLOAK_CLIENT_ID" is required');
-	}
-
-	if (!value.KEYCLOAK_SECRET) {
-		errors.push('"KEYCLOAK_SECRET" is required');
-	}
-
-	if (errors.length > 0) {
-		return helpers.error('any.custom', {
-			message: errors.join('. ')
-		});
-	}
-
-	return value;
-}, 'Keycloak configuration validation');
+});
