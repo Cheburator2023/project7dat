31e46cc7373ddede87775a2338d78a8d3b054280
VGabbasov <VGabbasov@inno.tech>
2025-11-28 14:51:04 +0300

    feat: Добавил остальные сущности

--- apps/nestjs-server/src/modules/json-data/json-data.module.ts
+++ apps/nestjs-server/src/modules/json-data/json-data.module.ts
@@ -14,6 +14,16 @@ import { AttributeMapEntity } from "./entities/attribute-map.entity";
 import { AttributeMapSourceEntity } from "./entities/attribute-map-source.entity";
 import { EntityAttributeMapEntity } from "./entities/entity-attribute-map.entity";
 import { FailedMappingsEntity } from "./entities/failed-mappings.entity";
+import { EntityTypeEntity } from "./entities/entity-type.entity";
+import { EntityContainerTypeEntity } from "./entities/entity-container-type.entity";
+import { EntityContainerEntity } from "./entities/entity-container.entity";
+import { AttributeTypeEntity } from "./entities/attribute-type.entity";
+import { ProcessTypeEntity } from "./entities/process-type.entity";
+import { ProcessGroupEntity } from "./entities/process-group.entity";
+import { DependencyTypeEntity } from "./entities/dependency-type.entity";
+import { SystemsEntity } from "./entities/systems.entity";
+import { StreamSpaceEntity } from "./entities/stream-space.entity";
+import { EntityMapSourceEntity } from "./entities/entity-map-source.entity";
 
 // Services
 import { JsonDataService } from "./services/json-data.service";
@@ -30,8 +40,6 @@ import { AttributeTypeService } from "./services/attribute-type.service";
 import { EntityContainerService } from "./services/entity-container.service";
 import { DependencyCheckService } from "./services/dependency-check.service";
 import { VersioningService } from "./services/versioning.service";
-
-// New Validation Services
 import { JsonStructureValidationService } from "./services/json-structure-validation.service";
 import { JsonIntegrityValidationService } from "./services/json-integrity-validation.service";
 import { JsonBusinessRulesValidationService } from "./services/json-business-rules-validation.service";
@@ -71,6 +79,16 @@ export class JsonDataModule {
             AttributeMapSourceEntity,
             EntityAttributeMapEntity,
             FailedMappingsEntity,
+            EntityTypeEntity,
+            EntityContainerTypeEntity,
+            EntityContainerEntity,
+            AttributeTypeEntity,
+            ProcessTypeEntity,
+            ProcessGroupEntity,
+            DependencyTypeEntity,
+            SystemsEntity,
+            StreamSpaceEntity,
+            EntityMapSourceEntity,
         ];
 
         const imports = [
