742b377f00ea8d456fa15949afdb91757b338e1a
VGabbasov <VGabbasov@inno.tech>
2026-01-28 18:57:43 +0300

    fix: поправил реализацию валидации

--- apps/nestjs-server/src/modules/json-data/services/json-integrity-validation.service.ts
+++ apps/nestjs-server/src/modules/json-data/services/json-integrity-validation.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { IntegrityResult } from "../types/validation.types";
-import { IJsonIntegrityValidator } from "./interfaces/validation.interfaces";
+import { IntegrityResult } from "../types";
+import { JsonIntegrityValidator } from "./interfaces/validation.interfaces";
 
 @Injectable()
-export class JsonIntegrityValidationService implements IJsonIntegrityValidator {
+export class JsonIntegrityValidationService extends JsonIntegrityValidator {
     private readonly logger = new Logger(JsonIntegrityValidationService.name);
 
     validateIntegrity(data: any): IntegrityResult {
